d44fb506820aa815a133f7d7b1378892f7467e5b
Ryan Schaffer <rys686@mail.usask.ca>
2024-10-08 08:50:12 -0600

    added post-processing and assignment-2 sketch, 009 sketch

---
+++ src/bin/009-movable-objects.rs
@@ -0,0 +1,49 @@
+use bevy::app::{App, Startup};
+use bevy::asset::Assets;
+use bevy::color::Color;
+use bevy::DefaultPlugins;
+use bevy::math::Vec3;
+use bevy::pbr::{PbrBundle, StandardMaterial};
+use bevy::prelude::*;
+use bevy::render::mesh::VertexAttributeValues;
+use wasm_bindgen::prelude::wasm_bindgen;
+use web_demos::{DefaultPluginsWithCustomWindow};
+use web_demos::player::PlayerPlugin;
+
+#[wasm_bindgen(js_name = sourceFile)]
+pub fn source_file() -> String { include_str!("009-movable-objects.rs").to_string() }
+#[wasm_bindgen(js_name = demoName)]
+pub fn demo_name() -> String { "Pickup Objects".to_string() }
+fn main() {
+    #[cfg(not(target_arch = "wasm32"))]
+    start_game();
+}
+#[wasm_bindgen(js_name = startGame)]
+pub fn start_game() {
+    App::new()
+        .add_plugins(DefaultPluginsWithCustomWindow)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(4.0, 2.0, 4.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+
+    // Basic cube mesh
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(Cuboid::default())),
+        material: materials.add(StandardMaterial {
+            base_color: Color::srgb(0.8, 0.0, 0.0),
+            ..default()
+        }),
+        ..default()
+    });
+}
